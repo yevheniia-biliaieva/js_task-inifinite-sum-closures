1a49f36d4005f197eb574d9914d9f140e29688be
Yevheniia Biliaieva <biliaieva99@gmail.com>
2025-12-17 17:19:20 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,25 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  const adder = (a) => {
+    if (a === undefined) {
+      return 0;
+    }
+
+    return (b) => {
+      let result = a;
+
+      if (b === undefined) {
+        return result;
+      }
+
+      result += b;
+
+      return adder(result);
+    };
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
